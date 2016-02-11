243c18d7f9ea47e48c7092fed5e3b7ccf7d3353d
imichaelv <pjvanderveen@msn.com>
2016-02-11 15:29:39 +0100

    Opdracht 8 toegevoegd

---
+++ src/com/github/leertaken/leertaak1/opdracht8/DobbelsteenMVC.java
@@ -0,0 +1,47 @@
+package com.github.leertaken.leertaak1.opdracht8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DobbelsteenMVC extends JApplet {
+    DobbelsteenModel model;             //het model
+    TekstView tekstView;              // view
+    DobbelsteenView dobbelsteenView;  // view
+    DobbelsteenController controller;            // Controller
+    Statistieken statistieken ;
+
+    /*public static void main(String[] args){
+        init();
+    }*/
+
+    public void init() {
+        resize(250, 200);
+
+        // Maak het model
+        model = new DobbelsteenModel();
+
+        // Maak de controller en geef hem het model
+        controller = new DobbelsteenController(model);
+        controller.setBackground(Color.cyan);
+        getContentPane().add(controller, BorderLayout.NORTH);
+
+        // Maak de views
+        dobbelsteenView = new DobbelsteenView(Color.red);
+        dobbelsteenView.setBackground(Color.black);
+        getContentPane().add(dobbelsteenView, BorderLayout.CENTER);
+        tekstView = new TekstView();
+        tekstView.setBackground(Color.green);
+        getContentPane().add(tekstView, BorderLayout.SOUTH);
+        statistieken = new StatistiekenView();
+        statistieken.setBackground(Color.orange);
+        getContentPane().add(statistieken, BorderLayout.WEST);
+
+
+        // Registreer de views bij het model
+        model.addActionListener(tekstView);
+        model.addActionListener(dobbelsteenView);
+
+        // Eerste worp
+        model.gooi();
+    }
+}
---
+++ src/com/github/leertaken/leertaak1/opdracht8/Statistieken.java
@@ -0,0 +1,15 @@
+package com.github.leertaken.leertaak1.opdracht8;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Hp user on 10-2-2016.
+ */
+public class Statistieken extends JPanel {
+
+
+    public Statistieken(){
+
+    }
+}
