4e6c0504a798b2cbf1cc22bdcaa7023bf37ae862
imichaelv <pjvanderveen@msn.com>
2016-02-17 16:42:15 +0100

    Opdracht 9 Klaar

---
+++ src/com/github/leertaken/leertaak1/opdracht9/Main.java
@@ -0,0 +1,10 @@
+package com.github.leertaken.leertaak1.opdracht9;
+
+/**
+ * Created by Hp user on 17-2-2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println(Long.toString(3405691582L,16));
+    }
+}
