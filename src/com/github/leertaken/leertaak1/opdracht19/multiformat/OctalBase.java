aa91358e36411e9bb50bbb5f74e33885b5bb21b5
Sergen Nurel <sergen_nurel@hotmail.com>
2016-02-18 15:12:16 +0100

    acht naar oct

--- src/com/github/leertaken/leertaak1/opdracht19/multiformat/OctalBase.java
+++ src/com/github/leertaken/leertaak1/opdracht19/multiformat/OctalBase.java
@@ -4,8 +4,8 @@ package com.github.leertaken.leertaak1.opdracht19.multiformat;
  * Created by Hp user on 17-2-2016.
  */
 
-public class AchtBase extends Base {
-    public AchtBase() {
-        super("acht",8,"01234567");
+public class OctalBase extends Base {
+    public OctalBase() {
+        super("oct",8,"01234567");
     }
 }
--- src/com/github/leertaken/leertaak1/opdracht19/ui/Command.java
+++ src/com/github/leertaken/leertaak1/opdracht19/ui/Command.java
@@ -24,7 +24,7 @@ import java.io.*;
  * De main-klasse die leest en schrijft naar de console.
  * 
  * @author Brugali
- * @author Baljé
+ * @author Baljï¿½
  */
 public class Command {
   Calculator calc=new Calculator();
@@ -55,7 +55,7 @@ public class Command {
       else if(command.equals("dec")) calc.setBase(new DecimalBase());
       else if(command.equals("bin")) calc.setBase(new BinaryBase());
       else if(command.equals("hex")) calc.setBase(new HexBase());
-      else if(command.equals("acht"))calc.setBase(new AchtBase());
+      else if(command.equals("oct"))calc.setBase(new OctalBase());
       else if(command.equals("rat")) calc.setFormat(new RationalFormat());
       else if(command.equals("fixed")) calc.setFormat(new FixedPointFormat());
       else if(command.equals("float")) calc.setFormat(new FloatingPointFormat());
